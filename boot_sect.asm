e3c4a23e8d53982bb3f9a885c94b93b7f303285b
njal3001 <njal_telsto@hotmail.com>
2022-01-20 23:21:18 +0100

    BIOS disk read

--- boot_sect.asm
+++ boot_sect.asm
@@ -3,13 +3,32 @@
 ; Start address
 [org 0x7c00]
 
-;
-; Print hex numbers to screen
-;
+; Initialize boot drive value
+mov [BOOT_DRIVE], dl
 
-mov bx, 0x0f9b
+; Initialize segment registers
+mov ax, cs
+mov ds, ax
+mov ss, ax
+mov es, ax
+
+; Move stack pointer to safe address
+mov bp, 0x8000
+mov sp, bp
+
+mov bx, ax
 call print_hex
-mov bx, 0xfab3
+
+; Select drive
+mov dl, [BOOT_DRIVE]
+; Number of sectors to read
+mov dh, 1
+; Buffer address
+mov bx, 0x9000
+
+call disk_load
+
+mov bx, [0x9000]
 call print_hex
 
 ; Infinite loop
@@ -17,8 +36,14 @@ jmp $
 
 %include "utils.asm"
 
+; Data
+BOOT_DRIVE: db 0
+
 ; Pad with 0 until 510 bytes
 times 510-($-$$) db 0
 
 ; Magic boot number at end
 dw 0xaa55
+
+times 256 dw 0xdada
+times 256 dw 0xface
--- utils.asm
+++ utils.asm
@@ -1,3 +1,5 @@
+; Input
+;   bx: Hex value
 print_hex:
     pusha
     
@@ -13,7 +15,7 @@ print_hex:
     mov al, 'x'
     int 0x10
 
-    print_loop:
+    print_hex_loop:
         ; Copy lower part of hex value
         mov al, bh
 
@@ -26,33 +28,119 @@ print_hex:
         ; Check if value is 0-9 or a-f
         cmp al, 0x3a
 
-        jl print_loop_end 
+        jl print_hex_loop_end 
 
         ; Add another offset if a-f
         add al, 0x27
 
-        print_loop_end:
+        print_hex_loop_end:
         ; Print character
         int 0x10
         
         ; Check if loop is done
         inc cx
         cmp cx, 4
-        je print_end
+        je print_hex_end
 
         ; Shift to next digit
         shl bx, 4
 
-        jmp print_loop
+        jmp print_hex_loop
     
-
-    print_end:
+    print_hex_end:
 
     ; Print newline and carriage return
-    mov al, 10
+    mov al, 0xa
     int 0x10
-    mov al, 13
+    mov al, 0xd
     int 0x10
 
     popa
     ret
+
+; Input
+;   bx: Memory address of string
+print_string:
+    pusha
+    
+    ; Set print character mode
+    mov ah, 0x0e
+
+    print_string_loop:
+        ; Get current character
+        mov al, [bx]
+
+        ; Check if character is null terminator
+        cmp al, 0
+        je print_string_end
+
+        ; Print character
+        int 0x10
+        
+        ; Increment string address
+        inc bx
+
+        jmp print_string_loop
+
+    print_string_end:
+
+    ; Print newline and carriage return
+    mov al, 0xa
+    int 0x10
+    mov al, 0xd
+    int 0x10
+
+    popa
+    ret
+
+; Input
+;   dl: Selected drive
+;   dh: Number of sectors to read
+;   bx: Buffer address pointer (es:bx)
+disk_load:
+    pusha
+
+    ; Save number of sectors requested for error comparison
+    push dx 
+
+    ; Set read sector mode
+    mov ah, 0x02
+    ; Set number of sectors to read
+    mov al, dh
+    ; Select cylinder 0
+    mov ch, 0x00
+    ; Select head 0
+    mov dh, 0x00
+    ; Read from sector 2, first sector after boot sector
+    mov cl, 0x02
+    
+    ; Send interrupt
+    int 0x13
+
+    ; Get number of sectors read
+    pop dx
+    
+    ; Error if carry flag is set
+    jc disk_load_error
+
+    ; Check if requested number of sectors were read
+    cmp dh, al
+    ; Error if not matching
+    jne disk_load_error
+
+    jmp disk_load_end
+
+    disk_load_error:
+    
+    mov bx, DISK_LOAD_ERROR_MSG
+    call print_string
+    mov bx, ax
+    call print_hex
+
+    disk_load_end:
+
+    popa
+    ret
+
+
+DISK_LOAD_ERROR_MSG: db "Disk read error!", 0
